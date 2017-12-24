c2ceadc03fea62616407a62e652db949f84f7997
Malcolm Gourdine <malcolmgourdine@gmail.com>
2017-12-23 22:35:56 -0500

    album content component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TitlePageComponent } from './title-page/title-page.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AlbumContentComponent } from './album-content/album-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitlePageComponent,
-    NavbarComponent
+    NavbarComponent,
+    AlbumContentComponent
   ],
   imports: [
     BrowserModule
